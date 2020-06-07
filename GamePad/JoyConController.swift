9a0dc882247197c4e0088050447a9c69b402c403
Marco Luglio <marcodejulho@gmail.com>
2020-06-07 01:38:08 -0300

    Adding joy-con file for future implementation

---
+++ GamePad/JoyConController.swift
@@ -0,0 +1,22 @@
+//
+//  JoyConController.swift
+//  GamePad
+//
+//  Created by Marco Luglio on 06/06/20.
+//  Copyright © 2020 Marco Luglio. All rights reserved.
+//
+
+import Foundation
+
+
+
+class JoyConController {
+
+	static let VENDOR_ID_NINTENDO:Int64 = 0x057E // 1406
+	static let CONTROLLER_ID_JOY_CON_LEFT:Int64 = 0x2006 // 8198
+	static let CONTROLLER_ID_JOY_CON_RIGHT:Int64 = 0x2007 // 8199
+	static let CONTROLLER_ID_SWITCH_PRO:Int64 = 0x2009 // 8201
+	// 0x0306 Wii Remote Controller RVL-003
+	// 0x0337 Wii U GameCube Controller Adapter
+
+}
